ecca0bc87c0f6edec1b5abee41e8e41e31bb6852
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2019-12-21 14:19:59 -0300

    basic code for initial routes and pages

---
+++ src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>main</h1>
+    </Container>
+  );
+}
---
+++ src/pages/Repository/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Repository() {
+  return (
+    <Container>
+      <h1>repository</h1>
+    </Container>
+  );
+}
