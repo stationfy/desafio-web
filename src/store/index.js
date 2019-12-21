cc3e8b128a214225b1219ddc2494a4b11cf1a3e7
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2019-12-21 19:34:08 -0300

    removing saga
    removing because sagas for not being necessary now

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,7 @@
-import { persistStore } from 'redux-persist';
-import createSagaMiddleware from 'redux-saga';
-
 import createStore from './createStore';
 
 import rootReducer from './modules/rootReducer';
-import rootSaga from './modules/rootSaga';
 
-const sagaMonitor =
-  process.env.NODE_ENV === 'development'
-    ? console.tron.createSagaMonitor()
-    : null;
-
-const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
-
-const middlewares = [sagaMiddleware];
-
-const store = createStore(rootReducer, middlewares);
-
-sagaMiddleware.run(rootSaga);
+const store = createStore(rootReducer, []);
 
 export { store };
--- src/store/modules/rootSaga.js
+++
@@ -1,7 +0,0 @@
-import { all } from 'redux-saga/effects';
-
-import repositories from './repositories/sagas';
-
-export default function* rootSaga() {
-  return yield all([repositories]);
-}
