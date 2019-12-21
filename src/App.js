5c88ffd80da46f39bbe8e32d7c600cf4a742cd8e
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2019-12-21 17:59:50 -0300

    add redux, saga and changed routes file
    moving Router history to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+
 import Routes from './routes';
+import history from './services/history';
+import { store } from './store';
 
 function App() {
-  return <Routes />;
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+      </Router>
+    </Provider>
+  );
 }
 
 export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import '../config/ReactotronConfig';
-import history from '../services/history';
 
 import Main from '../pages/Main';
 import Repository from '../pages/Repository';
 
 export default function Routes() {
   return (
-    <Router history={history}>
-      <Switch>
-        <Route path="/" exact component={Main} />
-        <Route path="/repository" component={Repository} />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/" exact component={Main} />
+      <Route path="/repositories" component={Repository} />
+    </Switch>
   );
 }
