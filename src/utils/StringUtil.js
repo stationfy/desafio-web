754b5e9c7f4aab7c60d2c0fa0ade4a5e51c7100b
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2019-12-23 01:55:59 -0300

    finish unit tests and Utils libs

--- src/utils/StringUtil.js
+++ src/utils/StringUtil.js
@@ -1,10 +1,20 @@
-export function formattedDescription(description) {
+/**
+ * Returns 'No description provided.' if description is null
+ * or the first 250 characters with '...'
+ * @param {String} description
+ */
+export function getFirst250CharsDescription(description) {
   return description
     ? `${String(description).substr(0, 250)}...`
     : 'No description provided.';
 }
 
-export function formattedTitle(title) {
+/**
+ * Returns the first 100 characters if title have more than that
+ * or the original title
+ * @param {String} title
+ */
+export function getFirst100CharsTitle(title) {
   return String(title).length > 100
     ? `${String(title).substr(0, 100)}...`
     : title;
