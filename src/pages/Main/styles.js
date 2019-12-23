d79ab024a20c8bbca348738b9c6563b86d25bd86
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2019-12-22 23:55:16 -0300

    finish Main page (repositories)

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background: rgba(100, 180, 200, 0.5);
   margin: 0 auto;
+  min-width: 100px;
+  max-width: 900px;
 `;
