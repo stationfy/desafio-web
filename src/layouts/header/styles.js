550bfe1a0a689ba7c01ed53877888933e7f8e417
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2019-12-23 01:52:39 -0300

    setting up a new background into headerLayout

--- src/layouts/header/styles.js
+++ src/layouts/header/styles.js
@@ -3,4 +3,10 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+  color: rgba(255, 255, 255, 0.65);
+  background-color: #24292e;
+  background-image: url('https://desktop.github.com/images/star-bg.svg'),
+    linear-gradient(#191c20, #24292e 15%);
+  background-repeat: repeat-x;
+  background-position: center 10, 0 0, 0 0;
 `;
