8618294dd1c6cdd25a70bfe28ced267dfbe877c3
Hugo Ladeira de Freitas <HUGO_LADEIRA@hotmail.com>
2019-12-23 01:51:58 -0300

    change css font

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
-
   * {
     margin: 0;
     padding: 0;
@@ -32,7 +30,7 @@ export default createGlobalStyle`
   }
 
   body, input, button {
-    font: 14px 'Roboto', sans-serif;
+    font: 14px 'Times New Roman', sans-serif;
   }
 
   a {
